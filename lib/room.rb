afb9996f519b3ffcee17f7d2933682f1f87924f5
Patrik Björklund <p.bjorklund@gmail.com>
2012-08-25 19:42:23 +0200

    Methods on room no longer defined in initialize

--- lib/room.rb
+++ lib/room.rb
@@ -1,29 +1,30 @@
 class Room
   def initialize args={}
     @players = []
+
     args.each do |k,v| 
       self.instance_variable_set("@#{k}".to_sym, v) 
     end
+  end
 
-    def method_missing m
-      instance_var = self.instance_variable_get("@#{m.to_s}")
-      instance_var ? instance_var : "Room does not have that"
-    end
+  def method_missing m
+    instance_var = self.instance_variable_get("@#{m.to_s}")
+    instance_var ? instance_var : "Room does not have that"
+  end
 
-    def add_player player
-      @players << player
-    end
+  def add_player player
+    @players << player
+  end
 
-    def remove_player player
-      @players.delete player
-    end
+  def remove_player player
+    @players.delete player
+  end
 
-    def present
-      %Q{
+  def present
+    %Q{
 \033[32m#{self.name}\033[0m
 #{self.description}
 Exits: [ #{self.exits.inject("") { |tot, el| el[0] + " " + tot }}]
 }
-    end
   end
 end
